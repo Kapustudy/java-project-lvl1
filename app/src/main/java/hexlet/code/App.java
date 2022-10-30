317ee2847c395198f86b4e8e3ae04412790ac533
Aleksei Kapustin <kapustinl.study@gmail.com>
2022-10-30 22:08:13 +0300

    add App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,40 @@
 package hexlet.code;
+import hexlet.code.games.Game;
+import hexlet.code.games.Greet;
+import hexlet.code.games.Even;
 
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Cli.interview();
+        Game game;
+        Game[] listOfGames = {new Greet(), new Even()};
+
+        game = choosingGame(listOfGames);
+        if (game != null) {
+            game.startGame();
+        }
+    }
+
+    private static Game choosingGame(Game[] listOfGames) {
+        int gameNumber = 0;
+        System.out.println("Please enter the game number and press Enter.");
+        if (listOfGames == null) {
+            return null;
+        }
+        for (int i = 0; i < listOfGames.length; i++) {
+            System.out.println((i + 1) + " - " + listOfGames[i].getName());
+        }
+        System.out.println("0 - Exit");
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            gameNumber = sc.nextInt();
+            System.out.println("Your choice: " + gameNumber);
+        }
+        switch (gameNumber) {
+            case 1 : return new Greet();
+            case 2 : return new Even();
+            default: return null;
+        }
     }
 }
---
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -0,0 +1,64 @@
+package hexlet.code.games;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Even implements Game {
+
+    private String playersName;
+    public String getName() {
+
+        return "Even";
+    }
+    private void greeting() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextLine()) {
+            playersName = sc.nextLine();
+            System.out.println("Hello, " + playersName);
+        }
+    }
+    private int getRandom() {
+        int maxNumber = 100000;
+        Random random = new Random();
+        return random.nextInt(maxNumber);
+    }
+
+    private String rightAnswer(int number) {
+        if (number % 2 == 0) {
+            return "yes";
+        }
+        return  "no";
+    }
+    public void startGame() {
+        greeting();
+        String answer = "";
+        int number;
+        int counter = 0;
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        Scanner sc = new Scanner(System.in);
+        while (counter != 3) {
+            number = getRandom();
+            System.out.println("Question: " + number);
+            if (sc.hasNextLine()) {
+                answer = sc.nextLine();
+                System.out.println("Your answer: " + answer);
+            }
+            if (answer.equals(rightAnswer(number))) {
+                System.out.println("Correct!");
+                counter++;
+            } else {
+                System.out.println("'"
+                                 + answer
+                                 + "'"
+                                 + " is wrong answer ;(. Correct answer was "
+                                 + "'"
+                                 + rightAnswer(number)
+                                 + "'");
+                System.out.println("Let's try again, " + playersName + "!");
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + playersName + "!");
+    }
+}
